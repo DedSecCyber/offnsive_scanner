7664fc27cd1783515bb8524fea95bb1abb6d466b
DedSec CyberTeam TH <37942979+DedSecCyber@users.noreply.github.com>
2018-09-24 13:45:38 +0700

    Add files via upload

---
+++ offnsive.py
@@ -0,0 +1,10 @@
+import os, sys, time
+from time import sleep as timeout
+
+#Banner
+os.system("figlet offnsive scann")
+print "scanner tool for web app security"
+print
+print "        01 < link scanner"
+print "        02 < sqli scanner"
+print "        03 < dsn scanner"
